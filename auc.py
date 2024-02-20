298223228148ff728d420f6c6a50796e577a3f34
borroot <inbox@borroot.com>
2024-02-20 21:39:28 +0100

    Add 9plot

--- auc.py
+++ auc.py
@@ -27,7 +27,7 @@ def compute_scores(file_train, file_test_self, file_test_anomaly, n, r):
 
 
 def compute_stats(scores, num_self, num_anomaly):
-    # compute all sensitivity and specificity value in O(n)
+    # compute all sensitivity and specificity values in O(n)
     sensitivities = []
     specificities = []
 
@@ -39,9 +39,11 @@ def compute_stats(scores, num_self, num_anomaly):
         sensitivities.append((num_anomaly - count_anomaly) / num_anomaly)
         specificities.append(1 - (cutoff_index - count_anomaly) / num_self)
 
-    sensitivities = [1] + sensitivities + [0]
-    specificities = [1] + specificities + [0]
+    # add (0, 0) and (1, 1) and clip because of rounding errors
+    sensitivities = np.clip([1] + sensitivities + [0], 0, 1)
+    specificities = np.clip([1] + specificities + [0], 0, 1)
 
+    # compute auc
     auc = sklearn.metrics.auc(specificities, sensitivities)
     return sensitivities, specificities, auc
 
@@ -50,7 +52,7 @@ def generate_plot(sensitivities, specificities, auc, file_train, file_test_self,
     plt.subplots(figsize=(8, 8))
     plt.style.use('bmh')
 
-    plt.plot(specificities, sensitivities, color='#0e1111')
+    plt.plot(specificities, sensitivities, color='orange')
     plt.plot([0, 1], [0, 1], '--', color='#0e1111')
 
     plt.xlabel('1 - specificity')
@@ -63,10 +65,46 @@ def generate_plot(sensitivities, specificities, auc, file_train, file_test_self,
     plt.show()
 
 
+def generate_9plot(results, file_train, file_test_self, file_test_anomaly, n):
+    fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+
+    # hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+
+    # create the actual plots
+    for index, (sensitivities, specificities, auc) in enumerate(results):
+        axs[index // 3, index % 3].plot(specificities, sensitivities, color='orange')
+        axs[index // 3, index % 3].plot([0, 1], [0, 1], '--', color='#0e1111')
+        axs[index // 3, index % 3].set_title(f'r = {index + 1}, AUC = {auc:.4f}')
+
+    # give labels two big labels
+    fig.supxlabel('1 - specificity')
+    fig.supylabel('sensitivity')
+
+    # give on big title
+    fig.suptitle(
+        f'Trained on {file_train}, Tested on {file_test_self} and {file_test_anomaly}, n = {n}'
+    )
+    fig.tight_layout() # makes it less tight lol
+
+    plt.savefig(f'images/9plot_{file_train}_{file_test_self}_{file_test_anomaly}.png')
+    plt.show()
+
+
 def compute_auc(file_train, file_test_self, file_test_anomaly, n, r):
     scores, num_self, num_anomaly = compute_scores(file_train, file_test_self, file_test_anomaly, n, r)
     sensitivities, specificities, auc = compute_stats(scores, num_self, num_anomaly)
     generate_plot(sensitivities, specificities, auc, file_train, file_test_self, file_test_anomaly, n, r)
+
+
+def compute_aucs(file_train, file_test_self, file_test_anomaly, n):
+    results = []
+    for r in range(1, 10):
+        scores, num_self, num_anomaly = compute_scores(file_train, file_test_self, file_test_anomaly, n, r)
+        results.append(compute_stats(scores, num_self, num_anomaly))
+
+    generate_9plot(results, file_train, file_test_self, file_test_anomaly, n)
 
 
 def main():
@@ -75,9 +113,9 @@ def main():
     file_test_anomaly = 'tagalog.test'
 
     n = 10
-    r = 4
+    r = 2
 
-    compute_auc(file_train, file_test_self, file_test_anomaly, n, r)
+    compute_aucs(file_train, file_test_self, file_test_anomaly, n)
 
 
 if __name__ == '__main__':
