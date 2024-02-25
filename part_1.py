5cc426640ca8e25997fa23d1ed8cbca06a62ef08
Petre, V. (Victor) <victor.petre@ru.nl>
2024-02-25 23:31:38 +0100

    finish implementing part 1, implement part 2

--- part_1.py
+++ part_1.py
@@ -3,18 +3,17 @@ import numpy as np
 import os
 import sklearn.metrics
 
-
-def compute_matches(file_train, file_test, n, r):
+def compute_matches(file_train, file_test, alphabet, n, r):
     # train using the given training file, and compute the scores for the given test file
-    cmd = f'java -jar negsel2.jar -self {file_train} -n {n} -r {r} -c -l < {file_test}'
+    cmd = f'java -jar negsel2.jar -alphabet {alphabet} -self {file_train} -n {n} -r {r} -c -l < {file_test}'
     p = os.popen(cmd)
     return list(map(float, p.read().strip().split('\n')))  # parse as list of floats
 
 
-def compute_scores(file_train, file_test_self, file_test_anomaly, n, r):
+def compute_scores(file_train, alphabet, file_test_self, file_test_anomaly, n, r):
     # compute the self and anomaly scores using the java program
-    scores_self = compute_matches(file_train, file_test_self, n, r)
-    scores_anomaly = compute_matches(file_train, file_test_anomaly, n, r)
+    scores_self = compute_matches(file_train, file_test_self, alphabet, n, r)
+    scores_anomaly = compute_matches(file_train, file_test_anomaly, alphabet, n, r)
 
     # label the scores as self/anomaly, i.e. false/true
     scores = np.array(
@@ -33,13 +32,31 @@ def compute_stats(scores, num_self, num_anomaly):
 
     print(*scores, sep='\n')
 
+    # Approach: compute sensitivity and specificity as normal, unless there are
+    # multiple entries sharing the same score next to each other. If there are,
+    # only compute the sensitivity and specificity after sweeping through to
+    # the largest index of that "block" of same-score datapoints. This
+    # reflects what happens in actual classification; a shift in the threshold
+    # affects the outcome for all datapoints with that score at once. 
+
     count_anomaly = 0
-    for cutoff_index, [score, anomalous] in enumerate(scores):
+    for cutoff_index in range(len(scores)):
+        score, anomalous = scores[cutoff_index]
+        
+        if cutoff_index < len(scores) -1:
+            next_score, _ = scores[cutoff_index + 1]
+        else:
+            next_score = -1
+
         if anomalous:
             count_anomaly += 1
 
-        sensitivities.append((num_anomaly - count_anomaly) / num_anomaly)
-        specificities.append(1 - (cutoff_index - count_anomaly) / num_self)
+        # Checking if we're currently at the end of the "block" of same-score
+        # datapoints
+        if score != next_score:
+            sensitivities.append((num_anomaly - count_anomaly) / num_anomaly)
+            specificities.append(1 - (cutoff_index - count_anomaly) / num_self)
+
 
     # add (0, 0) and (1, 1) and clip because of rounding errors
     sensitivities = np.clip([1] + sensitivities + [0], 0, 1)
@@ -51,8 +68,8 @@ def compute_stats(scores, num_self, num_anomaly):
 
 
 def generate_plot(sensitivities, specificities, auc, file_train, file_test_self, file_test_anomaly, n, r):
-    plt.subplots(figsize=(8, 8))
-    plt.style.use('bmh')
+    plt.figure(figsize=(5, 5))
+
 
     plt.plot(specificities, sensitivities, color='orange')
     plt.plot([0, 1], [0, 1], '--', color='#0e1111')
@@ -60,10 +77,14 @@ def generate_plot(sensitivities, specificities, auc, file_train, file_test_self,
     plt.xlabel('1 - specificity')
     plt.ylabel('sensitivity')
 
+    if 'lang/' in file_test_anomaly:
+        file_test_anomaly = file_test_anomaly[5:]
+
     plt.title(
         f'Trained on {file_train}, Tested on {file_test_self} and {file_test_anomaly}\n\
         Using n = {n} and r = {r}, AUC = {auc:.4f}'
     )
+    plt.savefig(f'images/{file_train}_{file_test_self}_{file_test_anomaly}.png', bbox_inches="tight", dpi=300)
     plt.show()
 
 
@@ -90,36 +111,46 @@ def generate_9plot(results, file_train, file_test_self, file_test_anomaly, n):
     )
     fig.tight_layout() # makes it less tight lol
 
-    plt.savefig(f'images/9plot_{file_train}_{file_test_self}_{file_test_anomaly}.png')
+    plt.savefig(f'images/9plot_{file_train}_{file_test_self}_{file_test_anomaly}.png', bbox_inches="tight", dpi=300)
     plt.show()
 
 
-def compute_auc(file_train, file_test_self, file_test_anomaly, n, r):
-    scores, num_self, num_anomaly = compute_scores(file_train, file_test_self, file_test_anomaly, n, r)
+def compute_auc(file_train, alphabet, file_test_self, file_test_anomaly, n, r):
+    scores, num_self, num_anomaly = compute_scores(
+        file_train, alphabet, file_test_self, file_test_anomaly, n, r)
     sensitivities, specificities, auc = compute_stats(scores, num_self, num_anomaly)
-    generate_plot(sensitivities, specificities, auc, file_train, file_test_self, file_test_anomaly, n, r)
+    generate_plot(
+        sensitivities, specificities, auc, file_train, file_test_self, file_test_anomaly, n, r)
 
 
-def compute_aucs(file_train, file_test_self, file_test_anomaly, n):
+def compute_aucs(file_train, alphabet, file_test_self, file_test_anomaly, n):
     results = []
     for r in range(1, 10):
-        scores, num_self, num_anomaly = compute_scores(file_train, file_test_self, file_test_anomaly, n, r)
+        scores, num_self, num_anomaly = compute_scores(
+            file_train, alphabet, file_test_self, file_test_anomaly, n, r)
         results.append(compute_stats(scores, num_self, num_anomaly))
 
     generate_9plot(results, file_train, file_test_self, file_test_anomaly, n)
 
 
-def main():
+def part_1(n, r):
+    # Control: English vs Tagalog
     file_train = 'english.train'
     file_test_self = 'english.test'
     file_test_anomaly = 'tagalog.test'
+    alphabet = 'file://english.train'
+    compute_aucs(file_train, alphabet, file_test_self, file_test_anomaly, n)
 
-    n = 10
-    r = 1
+    other_langs = ['lang/hiligaynon.txt', 
+                   'lang/middle-english.txt',
+                   'lang/plautdietsch.txt',
+                   'lang/xhosa.txt']
 
-    compute_auc(file_train, file_test_self, file_test_anomaly, n, r)
-    # compute_aucs(file_train, file_test_self, file_test_anomaly, n)
+    for lang in other_langs:
+        compute_auc(file_train, alphabet, file_test_self, lang, n, r)
+
+
 
 
 if __name__ == '__main__':
-    main()
+    part_1(10,3)
